c1b0956b14eeed18f3f5e5a30ca3f37f902daf22
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-09-04 14:12:48 -0400

    Add files via upload

---
+++ verdu-cop3330-ex01/src/main/java/Exercise01/Exercise01.java
@@ -0,0 +1,23 @@
+package Exercise01;
+/*
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Evan Verdu
+ */
+
+import  java.util.Scanner;
+
+public class Exercise01 {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + ", nice to meet you!");
+
+    }
+
+}
